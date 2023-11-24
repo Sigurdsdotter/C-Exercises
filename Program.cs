74b251b17a68698b80ba12180569c1f86d6c2b21
Amanda Copa <amandacopa@gmail.com>
2023-11-24 13:13:56 +0100

    teachers exercises

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+
+namespace calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter the first number: ");
+        double number1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number: ");
+        double number2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Choose the operation: ");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtractio");
+        Console.WriteLine("3. Multiplication");
+
+        int choice = int.Parse(Console.ReadLine());
+
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine(number1 + number2);
+                break;
+            case 2:
+                Console.WriteLine(number1 - number2);
+                break;
+            case 3:
+                Console.WriteLine(number1 * number2);
+                break;
+            default:
+                Console.WriteLine("Write 1, 2 or 3 please");
+                break;
+        }
+
+
+    }
+}
+
