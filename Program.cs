45c6a926527d4e24872997b395c8c1961950b0fe
Amanda Copa <amandacopa@gmail.com>
2023-11-27 15:05:39 +0100

    Uppdate

--- Program.cs
+++ Program.cs
@@ -1,43 +1,56 @@
-﻿using Microsoft.VisualBasic;
+﻿using System;
 
-namespace calculator;
-
-class Program
+namespace Calculator
 {
-    static void Main(string[] args)
+    class Program
     {
-
-        Console.WriteLine("Enter the first number: ");
-        double number1 = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Enter the second number: ");
-        double number2 = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Choose the operation: ");
-        Console.WriteLine("1. Addition");
-        Console.WriteLine("2. Subtractio");
-        Console.WriteLine("3. Multiplication");
-
-        int choice = int.Parse(Console.ReadLine());
-
-
-        switch (choice)
+        static void Main(string[] args)
         {
-            case 1:
-                Console.WriteLine(number1 + number2);
-                break;
-            case 2:
-                Console.WriteLine(number1 - number2);
-                break;
-            case 3:
-                Console.WriteLine(number1 * number2);
-                break;
-            default:
-                Console.WriteLine("Write 1, 2 or 3 please");
-                break;
+
+            Console.WriteLine("Enter the first number: ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose the operation: ");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case 2:
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case 3:
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case 4:
+                    try
+                    {
+                        if (number2 == 0)
+                        {
+                            throw new DivideByZeroException("Sorry, I can't divide by 0");
+                        }
+                        Console.WriteLine(number1 / number2);
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Write 1, 2, 3, or 4 please");
+                    break;
+            }
         }
-
-
     }
 }
 
